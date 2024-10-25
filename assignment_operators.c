90ae8336770458d481b7662680cac0ec5a7c568b
shrivaradb <shrivarad.bhagwatikar@gmail.com>
2024-10-26 00:13:18 +0530

    Add files via upload
    Here I have attached the files about the topics related to C Language.
    Name: Shrivarad Bhagwatikar
    Roll No: 16
    Division: A

---
+++ assignment_operators.c
@@ -0,0 +1,24 @@
+    #include<stdio.h>
+
+    int main()
+    {
+    int a;
+    a=5;
+
+    printf("Value of a is %d\n", a);
+
+    a += 5;
+    printf("Value of a is %d\n", a);
+
+    a -= 5;
+    printf("Value of a is %d\n", a);
+
+    a *= 5;
+    printf("Value of a is %d\n", a);
+
+    a /= 5;
+    printf("Value of a is %d\n", a);
+
+    return 0;
+
+}
