5cc6e303b3224540e48423064f8e1b5795959a51
shrivaradb <shrivarad.bhagwatikar@gmail.com>
2024-10-26 22:53:59 +0530

    Update relational_operators.c

--- relational_operators.c
+++ relational_operators.c
@@ -15,34 +15,6 @@ int main() {
     printf("a <= b: %d\n", a <= b);
     g=a==b;
     printf("a == b: %d\n", a == b);
-
-    // if else is used to get the output.
-
-     if (a > b)
-        printf("a is greater than b\n");
-    else
-        printf("b is greater than a\n");
-
-         if (a < b)
-        printf("a is less than b\n");
-    else
-        printf("a is greater than b\n");
-
-         if (a >= b)
-        printf("a is greater than or equal to b\n");
-    else
-        printf("a is less than b\n");
-
-         if (a <= b)
-        printf("a is lesser than or equal to b\n");
-    else
-        printf("a is greater than b\n");
-        
-         if (a == b)
-        printf("a is equal to b\n");
-    else
-        printf("a is not equal b\n");
-        
        
     return 0;
 }
