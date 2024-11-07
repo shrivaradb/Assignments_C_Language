53aca2921a8830ca2c458b872a119a0df3b9c685
Shrivarad Bhagwatikar <shrivarad.bhagwatikar@gmail.com>
2024-11-07 20:30:14 +0530

    Add files via upload

---
+++ Control Statements/Loops/for loop/even_no.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,i;
+
+    printf("Enter a number: ");
+    scanf("%d",&a);
+
+    for(i=0;i<=a;i++)
+    {
+        if(i%2==0)
+        
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
