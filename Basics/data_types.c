5c495c238176b434de6db77b2e0c9a50ad56caa1
Shrivarad Bhagwatikar <shrivarad.bhagwatikar@gmail.com>
2024-11-07 20:21:09 +0530

    Add files via upload

---
+++ Basics/data_types.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;//Data Type- Integer
+    a=16;
+    printf("Integer= %d\n",a);
+    
+    float b;//Data Type- Float
+    b=66;
+    printf("Float= %f\n",b);
+
+    char c;//Data Type- Character
+    c='x';
+    printf("Character= %c\n",c);
+
+
+    return 0;
+
+}
